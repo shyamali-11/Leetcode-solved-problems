cfda1933db3de1b559374c867a9b1b60def84716
shyamali-11 <73998034+shyamali-11@users.noreply.github.com>
2024-03-11 17:21:51 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+         for(int i=0;i<a.length;i++){
+            for(int j=i+1;j<a.length;j++){
+                if(a[i]+a[j]==k){
+                    c[0]=i;
+                c[1]=j;}
+                
+            }
+        }
+        return c;
+    }
+}
